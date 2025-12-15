c555799f33e3c44eabb3cf2b9f5644fa8cf3aa1a
Challelign Tilahun <chalie2123@gmail.com>
2025-12-15 16:50:53 +0300

    sign up form

---
+++ schemas/auth.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const authSchema = z.object({
+  name: z.string().min(2, {
+    message: "Name must be at least 2 characters.",
+  }),
+  email: z.email(),
+  password: z.string().min(6).max(20),
+});
