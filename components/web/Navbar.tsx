292862cf4592f529b2666e295326ed8a8dba5071
Challelign Tilahun <chalie2123@gmail.com>
2025-12-15 14:09:44 +0300

    Navbar and theme mode creation completed

---
+++ components/web/Navbar.tsx
@@ -0,0 +1,42 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { buttonVariants } from "../ui/button";
+import { ThemeToggle } from "./ThemeToggle";
+
+export const Navbar = () => {
+  return (
+    <nav className="w-full py-5 flex items-center justify-between">
+      <div className="flex items-center gap-4">
+        <Link href="/">
+          <h1 className="text-3xl font-bold">
+            Next <span className="text-blue-500">Pro</span>
+          </h1>
+        </Link>
+        <div className="flex items-center gap-2">
+          <Link className={buttonVariants({ variant: "ghost" })} href="/">
+            Home
+          </Link>
+          <Link className={buttonVariants({ variant: "ghost" })} href="/blog">
+            Blog
+          </Link>
+          <Link className={buttonVariants({ variant: "ghost" })} href="/create">
+            Create
+          </Link>
+        </div>
+      </div>
+      <div className="flex items-center gap-2 ">
+        <Link className={buttonVariants()} href="/auth/sign-up">
+          Sign up
+        </Link>
+        <Link
+          className={buttonVariants({ variant: "outline" })}
+          href="/auth/login"
+        >
+          Login
+        </Link>
+        <ThemeToggle />
+      </div>
+    </nav>
+  );
+};
